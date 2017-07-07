02225f797eafb16660c87ab767bd48faa521d30b
Tyson Gern <tgern@pivotal.io>
2017-07-07 09:52:54 -0600

    Enable circuit breaker

--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -10,21 +10,23 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class ProjectClient {
 
-    private static final Logger logger = LoggerFactory.getLogger( ProjectClient.class );
-    private final RestOperations restOperations;
-    private final String registrationServerEndpoint;
+    private final Logger logger = LoggerFactory.getLogger(getClass());
     private final Map<Long, ProjectInfo> projectsCache = new ConcurrentHashMap<>();
+    private final RestOperations restOperations;
+    private final String endpoint;
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
-        this.restOperations= restOperations;
-        this.registrationServerEndpoint = registrationServerEndpoint;
+        this.restOperations = restOperations;
+        this.endpoint = registrationServerEndpoint;
     }
 
     @CircuitBreaker(name = "project", fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        ProjectInfo projectInfo = restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
-        projectsCache.put(projectId, projectInfo);
-        return projectInfo;
+        ProjectInfo project = restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+
+        projectsCache.put(projectId, project);
+
+        return project;
     }
 
     public ProjectInfo getProjectFromCache(long projectId, Throwable cause) {
--- components/timesheets/src/main/java/io/pivotal/pal/tracker/timesheets/ProjectClient.java
+++ components/timesheets/src/main/java/io/pivotal/pal/tracker/timesheets/ProjectClient.java
@@ -11,6 +11,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ProjectClient {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
+    private final Map<Long, ProjectInfo> projectsCache = new ConcurrentHashMap<>();
     private final RestOperations restOperations;
     private final String endpoint;
     private final Map<Long, ProjectInfo> projectsCache = new ConcurrentHashMap<>();
@@ -22,9 +23,11 @@ public class ProjectClient {
 
     @CircuitBreaker(name = "project", fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        ProjectInfo projectInfo = restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
-        projectsCache.put(projectId, projectInfo);
-        return projectInfo;
+        ProjectInfo project = restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+
+        projectsCache.put(projectId, project);
+
+        return project;
     }
 
     public ProjectInfo getProjectFromCache(long projectId, Throwable cause) {
